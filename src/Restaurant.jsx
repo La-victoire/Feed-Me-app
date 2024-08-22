7474fae35db3345872ef0b6bf22526e92b2174b8
Franklyn <akanofranklyn@gmail.com>
2024-08-22 13:49:27 +0100

    fixed buttons

---
+++ src/Restaurant.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import App from './App'
+import { Header } from './Header'
+import {Banner} from './Hero'
+import Buttons from './Buttons'
+
+
+
+export const Restaurant = () => {
+  return (
+    <>
+    <Header/>
+    <Banner/>
+    <Buttons/>
+    <App/>
+
+
+    </>
+  )
+}
